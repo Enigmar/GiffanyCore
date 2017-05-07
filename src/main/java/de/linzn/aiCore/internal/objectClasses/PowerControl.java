d1674d6f1906f957f806f206e5653bec9270443c
Niklas Linz <niklaslinz@hotmail.com>
2017-05-07 11:38:06 +0200

    Power control

--- src/main/java/de/linzn/aiCore/internal/objectClasses/PowerControl.java
+++ src/main/java/de/linzn/aiCore/internal/objectClasses/PowerControl.java
@@ -6,7 +6,7 @@ import java.lang.reflect.InvocationTargetException;
 import de.linzn.aiCore.App;
 import de.linzn.aiCore.internal.IObjectClass;
 
-public class Plex implements IObjectClass {
+public class PowerControl implements IObjectClass {
 
 	@Override
 	public void runTask(String function) {
@@ -34,11 +34,30 @@ public class Plex implements IObjectClass {
 	}
 
 	public void restartRaspel3() {
-		App.logger("Restart raspel");
+		App.logger("Restart raspel3");
 		restartUniversal("10.40.0.10", 22, "root");
 	}
 	
+	public void wakeNiklasPC(){
+		App.logger("Wakeup NiklasPC");
+		wakeOnLan("4c:cc:6a:0e:16:58");
+	}
+	
+	
+	//
+	
+	private void wakeOnLan(String mac){
+		//Need "apt-get install etherwake" packet installed
+		try {
+			Runtime.getRuntime().exec("etherwake " + mac).waitFor();
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	private void restartUniversal(String ip, int port, String user){
+		//Need deposit ssh key first
 		try {
 			Runtime.getRuntime().exec("ssh " + user + "@" + ip + " -p " + port + " 'reboot'").waitFor();
 		} catch (IOException | InterruptedException e) {
