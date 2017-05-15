30c6df0526722840a53e31202c82a753e9b27c02
Niklas <niklaslinz@hotmail.com>
2017-05-15 10:53:02 +0200

    Performe wait

--- src/main/java/de/linzn/aiCore/internal/skillsApi/PowerControl.java
+++ src/main/java/de/linzn/aiCore/internal/skillsApi/PowerControl.java
@@ -1,13 +1,14 @@
 package de.linzn.aiCore.internal.skillsApi;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class PowerControl {
 
     public void wakeOnLan(String mac) {
         // Need "apt-get install etherwake" packet installed
         try {
-            Runtime.getRuntime().exec("etherwake " + mac).waitFor();
+            Runtime.getRuntime().exec("etherwake " + mac).waitFor(1000, TimeUnit.MILLISECONDS);
         } catch (IOException | InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -17,7 +18,7 @@ public class PowerControl {
     public void restartUniversal(String ip, int port, String user) {
         // Need deposit ssh key first
         try {
-            Runtime.getRuntime().exec("ssh " + user + "@" + ip + " -p " + port + " 'reboot'").waitFor();
+            Runtime.getRuntime().exec("ssh " + user + "@" + ip + " -p " + port + " 'reboot'").waitFor(1000, TimeUnit.MILLISECONDS);
         } catch (IOException | InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
