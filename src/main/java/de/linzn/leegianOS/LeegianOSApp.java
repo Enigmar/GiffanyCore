164b5c4c1b98669168b5ce0a3f7c37a148fa5948
Niklas <niklas.linz@hotmail.com>
2018-05-09 21:06:26 +0200

    Update logger formatting

--- src/main/java/de/linzn/leegianOS/LeegianOSApp.java
+++ src/main/java/de/linzn/leegianOS/LeegianOSApp.java
@@ -28,6 +28,7 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.FileHandler;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
@@ -85,17 +86,29 @@ public class LeegianOSApp {
         fileLogger = Logger.getLogger("leegianOS");
         fileLogger.setUseParentHandlers(false);
         FileHandler fh;
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy-HH:mm:ss");
 
         try {
             File logsDir = new File("logs");
             if (!logsDir.exists()) {
                 logsDir.mkdir();
             }
-            // This block configure the fileLogger with handler and formatter
+
             fh = new FileHandler("logs/" + dateFormat.format(new Date().getTime()) + ".log");
             fileLogger.addHandler(fh);
-            SimpleFormatter formatter = new SimpleFormatter();
+            SimpleFormatter formatter = new SimpleFormatter() {
+                private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+
+                @Override
+                public synchronized String format(LogRecord lr) {
+                    return String.format(format,
+                            new Date(lr.getMillis()),
+                            lr.getLevel().getLocalizedName(),
+                            lr.getMessage()
+                    );
+                }
+            };
+
             fh.setFormatter(formatter);
         } catch (SecurityException | IOException e) {
             e.printStackTrace();
